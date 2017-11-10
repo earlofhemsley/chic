f2052575a18b4d7ec147443252b4a3813ada4939
Landon Hemsley <lhemsley@egovmt.com>
2017-11-10 12:35:58 -0700

    single product page styled through from gallery to tabs

--- classes/class-woocommerce-single-product.php
+++ classes/class-woocommerce-single-product.php
@@ -7,6 +7,9 @@ class wcsc_content_single_product {
         //action hooks will be added here
         add_action('woocommerce_before_main_content', array($this, 'before_main_content'));
         add_action('woocommerce_after_main_content', array($this, 'after_main_content'));
+        add_action('woocommerce_single_product_summary', array($this, 'single_product_summary'), 25);
+        add_action('woocommerce_before_add_to_cart_button', array($this, 'before_add_to_cart_button'));
+        add_action('woocommerce_after_add_to_cart_button', array($this, 'after_add_to_cart_button'));
     }
 
 
@@ -22,7 +25,21 @@ class wcsc_content_single_product {
 
     public function before_single_product(){}
     
-    public function before_single_product_summary(){}
+    public function before_add_to_cart_button(){ ?>
+        <div class="wcsc-cart-add-form">
+            <div class="quantity-wrapper">
+    <?php }
+
+    public function after_add_to_cart_button(){ ?>
+            </div><!-- .quantity-wrapper -->
+        </div><!-- .wcsc-cart-add-form -->    
+    <?php }
+
+
+    public function single_product_summary(){
+        global $product;
+        //var_dump($product->get_type());
+    }
 
 
 
