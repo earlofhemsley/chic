d05f77eb4540b280a451a1886695a3366a6d6b10
Landon Hemsley <lhemsley@egovmt.com>
2017-11-22 08:36:36 -0700

    fixup! make images on product pages smaller on smaller non-mobile viewports

--- classes/class-woocommerce-archive.php
+++ classes/class-woocommerce-archive.php
@@ -6,8 +6,6 @@ class woocommerce_archive{
         add_action('woocommerce_before_shop_loop', array($this, 'before_shop_loop'), 5);
         add_action('woocommerce_after_shop_loop', array($this, 'after_shop_loop'), 100);
     
-        //filters
-        add_filter('woocommerce_show_page_title', array($this, 'show_page_title'));
     }
 
     public function before_shop_loop(){ ?>
@@ -18,9 +16,6 @@ class woocommerce_archive{
         </div><!-- .wcsc-shop-loop -->
     <?php }
 
-    public function show_page_title($show){ 
-        return false;
-    }
 
 }
 
