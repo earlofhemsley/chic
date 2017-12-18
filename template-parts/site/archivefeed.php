f6417a24da16bbad78952c43f6a99498ec35210f
Landon Hemsley <lhemsley@egovmt.com>
2017-12-18 08:08:23 -0700

    beginnings of styling for post archive loops

--- template-parts/site/archivefeed.php
+++ template-parts/site/archivefeed.php
@@ -1,6 +1,6 @@
 <li <?php post_class(); ?> id="post-<?php the_ID(); ?>">
     <a href="<?php the_permalink(); ?>">
-    <div class="sewchic-post" style="background-image:<?php echo common_get_feed_image_url(); ?>;"></div>
-        <?php the_title(); ?>
+    <div class="sewchic-loop-post_image" style="background-image: url('<?php echo common_get_feed_image_url(); ?>'); width:200px; height:200px;" ></div>
+        <h2 class="sewchic-loop-post_title"><?php the_title(); ?></h2>
     </a>
 </li>
