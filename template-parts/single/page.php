265d70967d61e78cf4245e3459f370c9a4e57090
Landon Hemsley <lhemsley@egovmt.com>
2017-12-22 07:58:33 -0700

    redo wp page template

--- template-parts/single/page.php
+++ template-parts/single/page.php
@@ -1,16 +1,23 @@
 <!--
 This template part is for single page, non-post content. It is a trimmed down, bare-bones approach to page content.
 -->
-<div id="header page-header">
-    <h1><?php the_title(); ?></h1>
-    <?php if(has_post_thumbnail(get_the_ID())): ?>
-        <?php $caption = get_post(get_post_thumbnail_id())->post_excerpt; ?>
-        <figure class="single-featured-image">
+<article id="page-<?php the_ID(); ?>" <?php post_class("header page-header"); ?>>
+    <h1 class="sewchic-single-title"><?php echo get_the_title(); ?></h1>
+<?php 
+    if(has_post_thumbnail(get_the_ID()) && common_is_on_page_one() ): 
+?>            
+        <figure class="sewchic-single-featured-image">
             <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'post-hero'); ?>" />
-            <?php if($caption) printf('<figcaption>%s</figcaption>',$caption); ?>    
+<?php
+            $caption = get_post(get_post_thumbnail_id())->post_excerpt;
+            if($caption) echo sprintf('<figcaption>%s</figcaption>', $caption);
+?>
         </figure>
-    <?php endif; ?>
-</div>
-<div class="content page-content">
-    <?php the_content(); ?>
-</div>
+<?php
+    endif;
+    printf('<div class="%s">%s</div>',
+        'sewchic-single-content sewchic-post',
+        apply_filters('the_content', get_the_content())
+    );
+?>
+</article>
