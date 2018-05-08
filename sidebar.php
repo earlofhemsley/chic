9d7e67dea5defb3a4e5a968c3697c926a0e291ee
Landon Hemsley <lhemsley@egovmt.com>
2018-05-07 22:39:55 -0600

    remove deprecated crap

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,5 @@
 <?php
 
-//if(is_home() || is_front_page()) { 
-//    //do nothing
-//}
-//    get_template_part('template-parts/home/sidebar');
 if(is_shop() || is_product_category() || is_product_tag() || is_archive() || is_search())
     get_template_part('template-parts/site/sidebar','shop');
 else
