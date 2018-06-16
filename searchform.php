138a8c454121a68084c476a1aa1b8f922237b3b4
Landon Hemsley <lhemsley@egovmt.com>
2018-06-16 11:54:49 -0600

    Explore -> Search

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 <div class="sewchic-header-widget-element sewchic-form-inline sewchic-form" id="sewchic_search_form">
     <form action="<?php echo get_home_url(); ?>" method="get" role="search">
         <div class="sewchic-input-group">
-            <label for="sewchic-search-input" class="big-text-sm"><?php _e('Explore', 'sewchic'); ?>:</label>
+            <label for="sewchic-search-input" class="big-text-sm"><?php _e('Search', 'sewchic'); ?>:</label>
             <input type="text" id="sewchic-search-input" class="input-text" name="s" />
             <button class="button" type="submit"><?php _e('Submit', 'sewchic'); ?></button>
         </div>
