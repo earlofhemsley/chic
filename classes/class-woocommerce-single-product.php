4b013d0e773e8d56fd4e66d1cc1627f74f79b059
Landon Hemsley <lhemsley@sofi.org>
2019-05-21 09:26:16 -0600

    update styles such that star reviews actually appear again

--- classes/class-woocommerce-single-product.php
+++ classes/class-woocommerce-single-product.php
@@ -75,8 +75,12 @@ class woocommerce_single_product {
         $rating = intval(get_comment_meta($comment->comment_ID, 'rating', true));
         echo '<p class="sewchic-wc-star-rating">';
         for($i = 1; $i<=5; $i++){
-            $class = 'glyphicon glyphicon-star';
-            if($i > $rating) $class .= '-empty';
+            $class = 'sewchic-wc-star';
+            if($i <= $rating) {
+                $class .= ' sewchic-wc-star-full';
+            } else {
+                $class .= ' sewchic-wc-star-empty';
+            }
             echo "<span class='$class'></span>";
         }
         echo '</p><!-- .sewchic-wc-star-rating -->';
